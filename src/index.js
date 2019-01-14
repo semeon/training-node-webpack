d7a786dd6757af456971e5a619cf6e255a87f008
semeon <semeon.peradze@gmail.com>
2019-01-14 13:50:20 -0800

    Update

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ console.log("This is the first line of code!");
 var template = "This is a text message from ";
 var name = "Nafanail";
 var message = template + " " + name + "!";
-console.log(message);
-
+console.log("1) " + message);
 
 
 var foo = function(name) {
   var template = "This is a text message from ";
-  var message = template + " " + name + "! ГОCПОДЬ ЖГИ!";
+  var message = template + " " + name + "! ГОCПОДЬ ЖГИ!!!!!";
 
   return message;
 }
 
-console.log( foo ("Nafanail") );
+console.log( "2) " + foo ("Nafanail") );
